e808a0fce1e879fdd7ad5524e1bda909a5595da3
moussaabakar03 <alimoussaabakar003@gmail.com>
2026-03-03 23:04:21 +0100

    first commit 'creation de l'app back-end (spring boot)'

---
+++ src/main/java/com/ipnet/TransIaApplication.java
@@ -0,0 +1,13 @@
+package com.ipnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransIaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ipnet/TransIaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
